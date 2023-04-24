85f5301a438c8610e7acd3ce3a44758fc5325a4b
maks <zgonnikov01@gmail.com>
2023-04-24 21:57:07 +0300

    updated token response

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -142,7 +142,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    #return {"access_token": access_token, "token_type": "bearer"}
+    return {access_token}
 
 @router.post("/create_user", response_model=UserDisplay)
 async def create_user(
